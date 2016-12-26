d31b653b037030af44ca4d34a63f9f9bd3dddb7e
batterootesting <zebattertest@mailinator.com>
2016-12-26 11:22:58 +1100

    Update tester.py

--- Windows/tester.py
+++ Windows/tester.py
@@ -1,40 +1,88 @@
 # wireless keyboard tester for windows
 # windows 10
+import Tkinter
 import pythoncom
 import pyHook
+import time
 
-def keystroke_pressed(key_pressed):
-    # uncomment to see key pressed
-    #print key_pressed.Key
-    #print key_pressed.Ascii
-    counter()
+class WKB_Tester(Tkinter.Tk):
+    def __init__(self, parent):
+        Tkinter.Tk.__init__(self, parent)
+        self.parent = parent
+        self.initialize()
 
-    # press 'Esc' key to quit
-    if key_pressed.Ascii == 27:
-        global running
-        running = False
+    def initialize(self):
+        self.grid()
 
-def counter():
-    counter.count += 1
+        self.count = 0
 
-hookmanager = pyHook.HookManager()
+        self.title_image_src = Tkinter.PhotoImage(file="title.gif")
 
-hookmanager.KeyDown = keystroke_pressed
+        self.title_image = Tkinter.Label(self, image=self.title_image_src, bd=0)
+        self.title_image.grid(row=0, column=0, columnspan=3, padx=20, pady=14)
 
-hookmanager.HookKeyboard()
+        self.num_of_keystrokes_label = Tkinter.Label(self, text="Number of Keystrokes: ", bg="#ffffff", font=("Helvetica", 12))
+        self.num_of_keystrokes_label.grid(row=1, column=0, padx=20, sticky=Tkinter.W)
 
-# start counter at 0
-# note pressing Esc counts as a key!
-counter.count = 0
+        self.num_of_keystrokes_var = Tkinter.StringVar()
+        self.num_of_keystrokes = Tkinter.Label(self, textvariable=self.num_of_keystrokes_var, bg="#ffffff", font=("Helvetica", 12))
+        self.num_of_keystrokes.grid(row=1, column=2, columnspan=2, padx=20)
 
-running = True
+        self.start_label = Tkinter.Label(self, text="Start date and time: ", bg="#ffffff", font=("Helvetica", 12))
+        self.start_label.grid(row=2, column=0, padx=20, sticky=Tkinter.W)
 
-while running:
-    pythoncom.PumpWaitingMessages()
-else:
-    hookmanager.UnhookKeyboard()
+        self.start_time_var = Tkinter.StringVar()
+        self.start_time = Tkinter.Label(self, textvariable=self.start_time_var, bg="#ffffff", font=("Helvetica", 12))
+        self.start_time.grid(row=2, column=1, columnspan=2, padx=20)
 
-print "\n"
-print "-------------------------"
-print "Key have been pressed " + str(counter.count) + " times"
-print "-------------------------"
+        self.last_keystroke_label = Tkinter.Label(self, text="Last keystroke date and time:", bg="#ffffff", font=("Helvetica", 12))
+        self.last_keystroke_label.grid(row=3, column=0, padx=20, sticky=Tkinter.W)
+
+        self.grid_rowconfigure(4, pad=15)
+
+        self.start_button = Tkinter.Button(self, text="Start", bg="lightgreen", command=self.start_counting, width=15)
+        self.start_button.grid(row=4, column=1)
+
+        self.stop_button = Tkinter.Button(self, text="Stop", command=self.stop_counting, width=15)
+        self.stop_button.grid(row=4, column=2, padx=10)
+
+    def on_keyboard_event(self, event):
+        print event.Key
+        #print key_pressed.Ascii
+
+        def counter(self):
+            self.count += 1
+            print self.count
+            # *****  this function causes the program to lock up *****
+            #self.num_of_keystrokes_var.set(str(self.count))
+
+        counter(self)
+
+        return True
+
+    # this works if I just remove the hookmanager stuff and call this function
+    def my_kb_event_test(self):
+        def counter(self):
+            self.count += 1
+
+        counter(self)
+        self.num_of_keystrokes_var.set(str(self.count))
+
+    def start_counting(self):
+        print "Starting counting..."
+        self.start_time_var.set(time.strftime("%b %d %Y %H:%M:%S", time.localtime()))
+        self.hookmanager = pyHook.HookManager()
+        self.hookmanager.KeyDown = self.on_keyboard_event
+        self.hookmanager.HookKeyboard()
+
+    def stop_counting(self):
+        print "Stopping counting..."
+        self.hookmanager.UnhookKeyboard()
+
+if __name__ == "__main__":
+    top = WKB_Tester(None)
+    top.title("Wireless Keyboard Battery Tester")
+    top.wm_iconbitmap("favicon.ico")
+    top.configure(background="#ffffff")
+
+    top.mainloop()
